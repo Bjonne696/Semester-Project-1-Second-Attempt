c150dbba781201a7104a61e5d1d99a1678b1798f
Bjorn_Tore <Bjonne696@gmail.com>
2022-08-28 20:43:25 +0200

    update

--- js/products.js
+++ js/products.js
@@ -1,10 +1,21 @@
 const baseUrl = "https://flower-power.one/wp-json/wc/store/products"
+const productContainer = document.querySelector(".products");
 
 async function getProducts(url){
     const response = await fetch(url);
     const products = await response.json();
-    console.log(products);
+    createHTML(products)
 }
 
 
 getProducts(baseUrl);
+
+function createHTML(products){
+    products.forEach(function(product){
+        console.log(product);
+        productContainer.innerHTML += `<div><h2>${product.name}</h2>
+        <img src="${product.images[0].src}" alt="${product.name}">
+        </div>`;
+    })
+}
+
